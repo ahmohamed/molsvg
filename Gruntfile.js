8f5cc91fff3e245363d54db8abe20bdc4c1f376d
ahmohamed <ahmohamed@user.noreply.github.com>
2016-10-25 15:04:59 +0200

    update jshint options.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,11 @@
 module.exports = function(grunt) {
-  "use strict";
+  'use strict';
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
     browserify: {
       dist: {
         files: {
-          "<%= pkg.name %>.js": ["src/main.js"]
+          '<%= pkg.name %>.js': ['src/main.js']
         }
       },
       options: {
@@ -20,22 +20,23 @@ module.exports = function(grunt) {
       },
       my_target: {
         files: {
-          "<%= pkg.name %>.min.js": ["<%= pkg.name %>.js"]
+          '<%= pkg.name %>.min.js': ['<%= pkg.name %>.js']
         }
       }
     },
     jshint: {
       all: ['Gruntfile.js', 'src/**/*.js'],
       options: {
+        jshintrc:true,
         reporter: require('jshint-stylish')
       }
     },
   });
   
-  grunt.loadNpmTasks("grunt-browserify");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask("dev", ["jshint", "browserify"]);
-  grunt.registerTask("default", ["dev", "uglify"]);
+  grunt.registerTask('dev', ['jshint', 'browserify']);
+  grunt.registerTask('default', ['dev', 'uglify']);
 };
